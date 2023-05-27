4a91bf67b25f508953396983429ffbee0bdce20e
kevinkim910408 <junho2413@gmail.com>
2023-05-27 07:34:44 -0400

    [FEAT] Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as RouterWrap } from "react-router-dom";
 import Router from "@/components/Routers/Router";
 import GlobalStyle from "@/assets/styles/GlobalStyle";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <RouterWrap>
       <Router />
       <GlobalStyle />
+      <Analytics />
     </RouterWrap>
   );
 };
