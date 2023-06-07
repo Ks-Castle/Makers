c7fc6a0acc3e3493f08c1044c92eddea239f7667
kevinkim910408 <junho2413@gmail.com>
2023-06-07 11:48:03 -0400

    [FIX] Meta Tag

--- src/components/UI/Head.tsx
+++ src/components/UI/Head.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Helmet } from "react-helmet-async";
 
 const Head = ({ link, desc }: { link: string; desc: string }) => {
@@ -7,8 +6,28 @@ const Head = ({ link, desc }: { link: string; desc: string }) => {
     <Helmet>
       <title>Makers: {link}</title>
       <meta name="description" content={`${desc}`} />
+
+      <meta property="og:title" content={`Makers: ${link}`} />
       <meta property="og:description" content={`${desc}`} />
+      <meta
+        property="og:image"
+        content="https://firebasestorage.googleapis.com/v0/b/maker-efebf.appspot.com/o/home%2FMetaImg.png?alt=media&token=b3a90484-eb5a-453a-bc06-b36d57e2810d&_gl=1*1gtqvud*_ga*MzU3Mzg1ODQ5LjE2ODI2MTYxNDQ.*_ga_CW55HF8NVT*MTY4NjA3MzM0MS42LjEuMTY4NjA3MzgzMS4wLjAuMA.."
+      />
+      <meta property="og:image:width" content="474" />
+      <meta property="og:image:height" content="187" />
+      <meta property="og:url" content="https://makers-plum.vercel.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={`Makers: ${link}`} />
+
+      <meta name="twitter:card" content={`Makers: ${link}`} />
+      <meta property="twitter:domain" content="makers-plum.vercel.app" />
+      <meta property="twitter:url" content="https://makers-plum.vercel.app" />
+      <meta name="twitter:title" content={`Makers: ${link}`} />
       <meta name="twitter:description" content={`${desc}`} />
+      <meta
+        name="twitter:image"
+        content="https://firebasestorage.googleapis.com/v0/b/maker-efebf.appspot.com/o/home%2FMetaImg.PNG?alt=media&token=73745d78-ebc2-4442-86c7-fdd7a82df5e5"
+      />
     </Helmet>
   );
 };
