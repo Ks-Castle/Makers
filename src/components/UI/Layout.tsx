f08fd253ea930df78906a58c8739ff0e8f1c7936
kevinkim910408 <junho2413@gmail.com>
2023-06-12 11:05:40 -0400

    [FEAT] Google Ads

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -1,10 +1,20 @@
 import flex from "@/assets/styles/flex";
+import GoogleAdvertise from "@/context/GoogleAds";
+import { infeedPropsOne } from "@/data/ads";
 import React from "react";
 import styled from "styled-components";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ContentsWrapper>
+      <div className="ads-row">
+        <GoogleAdvertise {...infeedPropsOne} />
+        <GoogleAdvertise {...infeedPropsOne} />
+        <GoogleAdvertise {...infeedPropsOne} />
+        <GoogleAdvertise {...infeedPropsOne} />
+        <GoogleAdvertise {...infeedPropsOne} />
+        <GoogleAdvertise {...infeedPropsOne} />
+      </div>
       <Contents>{children}</Contents>
     </ContentsWrapper>
   );
@@ -16,11 +26,14 @@ const ContentsWrapper = styled.div`
   ${flex({ direction: "column" })}
   height:calc(100vh - 60px);
   margin-top: 60px;
+  .ads-row {
+    ${flex({})}
+  }
 `;
 
 const Contents = styled.div`
   ${flex({})}
   width: 90%;
-  height: 100%;
+  height: 90%;
   padding: 1rem;
 `;
