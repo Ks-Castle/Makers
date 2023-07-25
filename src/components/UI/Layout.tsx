78349dc4a8f8ce836f1c93a4bd4c54871315a614
kevinkim910408 <junho2413@gmail.com>
2023-07-25 16:20:24 -0400

    Revert "[FEAT] Google Ads"
    This reverts commit f08fd253ea930df78906a58c8739ff0e8f1c7936.

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -1,20 +1,10 @@
 import flex from "@/assets/styles/flex";
-import GoogleAdvertise from "@/context/GoogleAds";
-import { infeedPropsOne } from "@/data/ads";
 import React from "react";
 import styled from "styled-components";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ContentsWrapper>
-      <div className="ads-row">
-        <GoogleAdvertise {...infeedPropsOne} />
-        <GoogleAdvertise {...infeedPropsOne} />
-        <GoogleAdvertise {...infeedPropsOne} />
-        <GoogleAdvertise {...infeedPropsOne} />
-        <GoogleAdvertise {...infeedPropsOne} />
-        <GoogleAdvertise {...infeedPropsOne} />
-      </div>
       <Contents>{children}</Contents>
     </ContentsWrapper>
   );
@@ -26,14 +16,11 @@ const ContentsWrapper = styled.div`
   ${flex({ direction: "column" })}
   height:calc(100vh - 60px);
   margin-top: 60px;
-  .ads-row {
-    ${flex({})}
-  }
 `;
 
 const Contents = styled.div`
   ${flex({})}
   width: 90%;
-  height: 90%;
+  height: 100%;
   padding: 1rem;
 `;
--- src/data/ads.ts
+++
@@ -1,7 +0,0 @@
-export const infeedPropsOne = {
-  format: "fluid",
-  layoutKey: "-i2-7+2w-11-86",
-  client: "ca-pub-9003965723849856",
-  slot: "8534418685",
-  responsive: "true",
-};
