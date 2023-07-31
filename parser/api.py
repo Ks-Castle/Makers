d15be4e8fdcbeaf8211242fd4baaa3c07a35e60f
kevinkim910408 <junho2413@gmail.com>
2023-07-31 17:53:18 -0400

    feat: selenium test

--- parser/api.py
+++ parser/api.py
@@ -1,3 +1,8 @@
-def get_info():
-    print("fetching start")
-    return "test"
+from selenium import webdriver
+
+url = "https://makers-plum.vercel.app/Home"
+
+browser = webdriver.Firefox()
+browser.get(url)
+
+print(browser)
