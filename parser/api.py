6ef9f6f6e0ede30ba54a9741359f16e6101e8d39
kevinkim910408 <junho2413@gmail.com>
2023-08-01 00:09:49 -0400

    feat: python scrapping logic

--- parser/api.py
+++ parser/api.py
@@ -1,14 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+import requests
+import bs4
 
-url = "https://maplestory.nexon.net/news"
-
-browser = webdriver.Firefox()
-browser.get(url)
-
-
-news_view_more_button = browser.find_element("#newsViewMore")
-news_view_more_button.click()
-
-browser.quit()
+def get_info():
+    whole_source= ""
+    arr = []
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
+        "Accept": "text_html,application_xhtml+xml,application_xml;q=0.9,image_webp,**/**;q=0.8"
+    }
+    URL = "https://maplestory.nexon.net/news"
+    response = requests.get(URL, headers=headers, verify=False)
+    whole_source = whole_source + response.text
+    soup = bs4.BeautifulSoup(whole_source, 'html.parser')
+    news_items = soup.select("body > div.global-content > div.component.component-news-filterable > div > div > div.news-wrapper > ul > li")
+    for item in news_items:
+        title = item.select_one("div.text > h3 > a").text
+        content = item.select_one("div.text > p:nth-child(2)").text
+        photo_style = item.select_one("div.photo")['style']
+        photo_url = photo_style.split('url(')[1][:-1]
+        link = "https://maplestory.nexon.net/" + item.select_one("div.text > h3 > a")['href']
+        time = item.select_one("div.text > p.timestamp").text
+        arr.append({
+            'title': title,
+            'content': content,
+            'time': time,
+            'photo': photo_url,
+            'link': "https://maplestory.nexon.net/" + link
+        })
+    return arr
--- parser/run.py
+++ parser/run.py
@@ -1,16 +1,37 @@
 import json
-from pprint import pprint
-from time import sleep as delay
 from api import get_info
 
+def read_product_file(file_path):
+    try:
+        with open(file_path, "r") as product_file:
+            return json.load(product_file)
+    except FileNotFoundError:
+        return []
+
 if __name__ == "__main__":
-    data = get_info()
-    delay(0.1)
-    updated = False
-    if len(data):
-        updated = True
-    if updated:
-        with open("../src/data/mockup/mapleEvent.json", "w") as product_file:
-            json.dump(data, product_file, ensure_ascii=False, indent=2)
+    newData = get_info()
+    existing_data = read_product_file("../src/data/mockup/newEvent.json")
+    if len(existing_data) == 0:
+        with open("../src/data/mockup/newEvent.json", "w") as product_file:
+            json.dump(newData, product_file, ensure_ascii=False, indent=2)
             product_file.write("\n")
+    else:
+        differences = []
+        field_to_compare = 'title'
+        for existing_item in existing_data:
+            found = False
+            for new_item in newData:
+                if new_item[field_to_compare] == existing_item[field_to_compare]:
+                    print(new_item[field_to_compare] == existing_item[field_to_compare])
+                    found = True
+                    break
+            if not found:
+                differences.append(existing_item)
+        oldData = newData + differences
+        with open("../src/data/mockup/oldEvent.json", "w") as product_file:
+                json.dump(oldData, product_file, ensure_ascii=False, indent=2)
+                product_file.write("\n")
+        with open("../src/data/mockup/newEvent.json", "w") as product_file:
+                json.dump(newData, product_file, ensure_ascii=False, indent=2)
+                product_file.write("\n")
     print("done!")
