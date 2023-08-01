7dd9f18d7f0e09598dc65f47d3c5c26053cf3b9c
kevinkim910408 <junho2413@gmail.com>
2023-08-01 00:32:15 -0400

    feat: add actual date logic

--- parser/api.py
+++ parser/api.py
@@ -1,8 +1,9 @@
+from datetime import datetime, timedelta
 import requests
 import bs4
 
 def get_info():
-    whole_source= ""
+    whole_source = ""
     arr = []
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
@@ -19,12 +20,24 @@ def get_info():
         photo_style = item.select_one("div.photo")['style']
         photo_url = photo_style.split('url(')[1][:-1]
         link = "https://maplestory.nexon.net/" + item.select_one("div.text > h3 > a")['href']
-        time = item.select_one("div.text > p.timestamp").text
+        time_str = item.select_one("div.text > p.timestamp").text
+        actual_date = get_relative_date(time_str)
         arr.append({
             'title': title,
             'content': content,
-            'time': time,
+            'time': time_str,
+            'actualDate': actual_date,  
             'photo': photo_url,
             'link': "https://maplestory.nexon.net/" + link
         })
     return arr
+
+def get_relative_date(date_str):
+    if "days ago" in date_str:
+        days_ago = int(date_str.split(" ")[0])
+        result_date = datetime.now() - timedelta(days=days_ago)
+        return result_date.strftime("%b %d, %Y")
+    elif "minutes" in date_str or "hours" in date_str:
+        return datetime.now().strftime("%b %d, %Y")
+    else:
+        return date_str
