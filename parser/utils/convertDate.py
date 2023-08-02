48e5a6d5323050c749f83212f4a76d0c3600cba4
kevinkim910408 <junho2413@gmail.com>
2023-08-02 00:17:19 -0400

    fix: date bug

--- parser/utils/convertDate.py
+++ parser/utils/convertDate.py
@@ -5,7 +5,7 @@ def get_relative_date(date_str):
         days_ago = int(date_str.split(" ")[0])
         result_date = datetime.now() - timedelta(days=days_ago)
         return result_date.strftime("%b %d, %Y")
-    elif "minutes" in date_str or "hours" in date_str:
+    elif "minutes" in date_str or "hours"  in date_str or "hour"  in date_str:
         return datetime.now().strftime("%b %d, %Y")
     else:
         return date_str
